4e1dec9ecd8477486b100e36492c394680526002
ChrisGrant1024 <chrisgrant1024@gmail.com>
2015-10-23 14:09:16 +1100

    Change Everything
    Now a formal website

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("footer").innerHTML = "<p>&copy; Chris Grant " + new Date().getFullYear() + ", All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML = 
+"<ul id='menu'>" +
+"<li class='navigation-main'><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
